6bffb219e94282bd6b6e7a65f9ceda01aa0f884e
colin.zuo <colin.zuo@cloudminds.com>
2021-01-02 12:06:59 +0800

    set max rate

--- docker-composes/ffmpeg/transcode_for_webrtc.sh
+++ docker-composes/ffmpeg/transcode_for_webrtc.sh
@@ -3,9 +3,7 @@ in_file=$1
 out_file=$2
 
 # https://github.com/FFmpeg/FFmpeg/blob/3fbc74582f9c3bb941fd63f59fdf25dd16a98a6c/libavcodec/options_table.h
-# https://github.com/FFmpeg/FFmpeg/blob/069d2b4a50a6eb2f925f36884e6b9bd9a1e54670/libavformat/options_table.h
-# options="-y -tune zerolatency -g 60 -max_delay 0 -bf 0 -flags:v +global_header -bsf:v "dump_extra=freq=keyframe" -profile:v main -level:v 2.0 -crf 23 -b:v 200K -an -c:v libx264 -r 25 -s 320x240"
-options="-y -tune zerolatency -g 60 -bf 0 -flags:v +global_header -bsf:v "dump_extra=freq=keyframe" -profile:v main -level:v 2.0 -crf 23 -b:v 200K -an -c:v libx264 -r 25 -s 320x240"
+options="-y -tune zerolatency -g 60 -bf 0 -flags:v +global_header -bsf:v "dump_extra=freq=keyframe" -profile:v high -level:v 2.0 -crf 23 -b:v 200K -maxrate 210K -bufsize 210K -an -c:v libx264 -r 15 -s 320x240"
 
 echo "ffmpeg -i ${in_file} ${options} ${out_file}"
 
